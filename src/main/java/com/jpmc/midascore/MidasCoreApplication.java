376f6e56b3f4755000f37bc4309b760c29374ac0
Hammad Ahmed <hammadahmed682003@gmail.com>
2026-04-06 10:47:47 +0100

    Task 2 - Completed

--- src/main/java/com/jpmc/midascore/MidasCoreApplication.java
+++ src/main/java/com/jpmc/midascore/MidasCoreApplication.java
@@ -2,12 +2,20 @@ package com.jpmc.midascore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import com.jpmc.midascore.foundation.Transaction;
 
 @SpringBootApplication
 public class MidasCoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MidasCoreApplication.class, args);
+    }
+
+    @KafkaListener(id="traderListener", topics="${general.kafka-topic}")
+    public void listen(Transaction transaction) {
+        System.out.println("Received Transaction: " + transaction);
     }
 
 }
